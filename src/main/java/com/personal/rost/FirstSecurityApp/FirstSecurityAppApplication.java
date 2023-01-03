f136e8510cc8f9c9bceb8cb0a546c244bdfad706
Rost <coderost@yandex.ru>
2023-01-03 20:53:47 +0300

    Refactor FirstSecurityAppApplication class via CommandLineRunner to try to index current people in DB.

--- src/main/java/com/personal/rost/FirstSecurityApp/FirstSecurityAppApplication.java
+++ src/main/java/com/personal/rost/FirstSecurityApp/FirstSecurityAppApplication.java
@@ -1,13 +1,23 @@
 package com.personal.rost.FirstSecurityApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.personal.rost.FirstSecurityApp.services.PeopleService;
+import lombok.AllArgsConstructor;
+
 @SpringBootApplication
-public class FirstSecurityAppApplication {
+@AllArgsConstructor
+public class FirstSecurityAppApplication implements CommandLineRunner {
+	private final PeopleService peopleService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSecurityAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		peopleService.index().forEach(System.out::println);
+	}
 }
