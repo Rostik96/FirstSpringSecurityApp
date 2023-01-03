8a0072f0d9ddfc36cc9d0c4a238be5d563226826
Rost <coderost@yandex.ru>
2023-01-03 20:50:33 +0300

    Add "Person" model

---
+++ src/main/java/com/personal/rost/FirstSecurityApp/models/Person.java
@@ -0,0 +1,45 @@
+package com.personal.rost.FirstSecurityApp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "person")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов")
+    @Column(name = "username")
+    private String username;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    @Column(name = "password")
+    private String password;
+
+    public Person(String username, int yearOfBirth) {
+        this.username = username;
+        this.yearOfBirth = yearOfBirth;
+    }
+}
